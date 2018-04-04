15a87ba28e3c8c4d816d88579ab78ed2e3fa70d1
IOSLAB INGENIERIA <ioslabingenieria@gmail.com>
2018-04-04 15:32:48 -0500

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by iMac on 04/04/18.
+//  Copyright © 2018 ChapuApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
